7c272a9396916926539ce0f542ec1411b103e7a6
wikigi git <wikigi.git@gmail.com>
2025-11-01 17:26:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const okBtn = document.getElementById('okBtn');
+const question = document.getElementById('question');
+const answerInput = document.getElementById('answer');
+const result = document.getElementById('result');
+
+const questions = [
+  { q: 'What is 2 + 2?', a: '4' },
+  { q: 'What color is the sky?', a: 'blue' },
+  { q: 'What is the capital of Canada?', a: 'ottawa' },
+  { q: 'How many legs does a spider have?', a: '8' },
+  { q: 'What is the opposite of hot?', a: 'cold' }
+];
+
